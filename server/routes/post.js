85c6a988964289f56e829e127472127d5adf1cc4
georgebastion <georgejacob437@gmail.com>
2022-06-04 17:36:55 +0300

    files

---
+++ server/routes/post.js
@@ -0,0 +1,8 @@
+import express, { response } from 'express';
+const router = express.Router();
+import {getPosts, createPost} from '../controllers/post.js'
+
+router.get('/', getPosts);
+router.post('/', createPost);
+
+export default router;
