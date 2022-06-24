997a4674766c969cdd70bff35c83494ce0f7f4f1
georgebastion <georgejacob437@gmail.com>
2022-06-24 10:20:14 +0300

    Pagination and Query

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -5,13 +5,13 @@ import PostMessage from "../models/postMessage.js";
 export const getPosts= async(req, res)=>{
     const { page } = req.query;
     try {
-        const LIMIT = 8;
+        const LIMIT = 4;
         const startIndex=(Number(page)-1)*LIMIT;
         const total = await PostMessage.countDocuments({});
         const posts = await PostMessage.find().sort({_id: -1}).limit(LIMIT).skip(startIndex);
 
         
-        res.status(200).json({data: posts, currentPage: Number(page), numberOfPageS: Math.ceil(total/LIMIT)});
+        res.status(200).json({data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total/LIMIT)});
     } catch (error) {
         res.status(404).json({message:error.message});
     }
